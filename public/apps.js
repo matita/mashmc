ef7e72c0d9dbc62726009a0b97e35e4533a4dbe6
Matita <matitald@gmail.com>
2015-04-24 12:51:11 +0200

    added autoprefixer, git info on package.json

--- public/apps.js
+++ public/apps.js
@@ -729,7 +729,7 @@ var mashmc = {
 require('./temp-apps.js');
 },{"./temp-apps.js":8,"./util/route.js":9}],8:[function(require,module,exports){
 (function() {
-  var app = {"name":"mashmc-apps","appName":"apps","version":"0.1.0","description":"Lists all installed apps","author":{"name":"Matita","email":"matitald@gmail.com"},"path":"c:\\matteo\\progetti\\mashmc\\node_modules\\mashmc-apps","url":"/apps","hasGUI":true,"jsPath":"c:\\matteo\\progetti\\mashmc\\node_modules\\mashmc-apps\\public\\app.js","html":"  <div class=\"main\">\r\n\r\n  </div>\r\n\r\n\r\n  <script class=\"tmpl-plugin\" type=\"text/html\">\r\n    <div class=\"plugin\">\r\n      <a href=\"{{url}}\"><h2 class=\"plugin-name\">{{appName}}</h2></a>\r\n      <p class=\"plugin-description\">{{description}}</p>\r\n    </div>\r\n  </script>"};
+  var app = {"name":"mashmc-apps","appName":"apps","version":"0.1.0","description":"Lists all installed apps","author":{"name":"Matita","email":"matitald@gmail.com"},"path":"c:\\matteo\\progetti\\mashmc\\node_modules\\mashmc-apps","url":"/apps","hasGUI":true,"jsPath":"c:\\matteo\\progetti\\mashmc\\node_modules\\mashmc-apps\\public\\app.js","html":"  <div class=\"main\">\r\n\r\n  </div>\r\n\r\n\r\n  <script class=\"tmpl-plugin\" type=\"text/html\">\r\n    <a href=\"{{url}}\">\r\n      <div class=\"mashmc-list-item plugin\">\r\n        <h2 class=\"plugin-name\">{{appName}}</h2>\r\n        <p class=\"plugin-description\">{{description}}</p>\r\n      </div>\r\n    </a>\r\n  </script>"};
   var AppFn = require('c:\\matteo\\progetti\\mashmc\\node_modules\\mashmc-apps\\public\\app.js');
   var util = require('util');
   var BaseApp = require('./mashmc-app.js')(app);
